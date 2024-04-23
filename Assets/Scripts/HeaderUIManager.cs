d55f654e252c76474e0019f619fa7a8e27bf68f1
Yinuo Zhou <zoez@usc.edu>
2024-04-22 23:44:03 -0700

    rehook headuiManager

--- Assets/Scripts/HeaderUIManager.cs
+++ Assets/Scripts/HeaderUIManager.cs
@@ -4,7 +4,7 @@ using UnityEngine.UI;
 using System.Collections.Generic;
 
 
-public class UIManager : MonoBehaviour
+public class HeaderUIManager : MonoBehaviour
 {
     [System.Serializable]
     public class SuitPanel
