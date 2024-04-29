6f3d6186fef741fdeb0e171826c5ab94cc34017e
Yinuo Zhou <zoez@usc.edu>
2024-04-28 20:53:08 -0700

    add canvasUIManager Script

---
+++ Assets/Scripts/CanvasUIManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+
+public class CanvasUIManager : MonoBehaviour
+{
+    public GameObject[] UIPanels; // NavigationSystem, DetailsPanel, Egress & Ingress Panels
+    public GameObject headerPanel;
+    public Toast toast;
+
+    [System.Serializable]
+    public class Toast
+    {
+        public TextMeshProUGUI toastText;
+        public GameObject toastGameObject;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HideAllPanels();
+        ShowHeaderPanel();
+        ShowPanel(0); //default to show egress, can be deleted after testing
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void HideAllPanels()
+    {
+        foreach (var panel in UIPanels)
+        {
+            panel.SetActive(false);
+        }
+    }
+
+    // 0 => EVATaskPanel
+    // 1 => NavigationSystem
+    // 2 => DetailsPanel
+    void ShowPanel(int panelIndex)
+    {
+        HideAllPanels();
+        UIPanels[panelIndex].SetActive(true);
+    }
+
+    void HidePanel(int panelIndex)
+    {
+        UIPanels[panelIndex].SetActive(false);
+    }
+
+    void ShowHeaderPanel()
+    {
+        headerPanel.SetActive(true);
+    }
+    void HideHeaderPanel()
+    {
+        headerPanel.SetActive(false);
+    }
+    void ShowToast(string toastMessage)
+    {
+        toast.toastGameObject.SetActive(true);
+        toast.toastText.text = toastMessage;
+    }
+    void HideToast()
+    {
+        toast.toastGameObject.SetActive(false);
+    }
+}
