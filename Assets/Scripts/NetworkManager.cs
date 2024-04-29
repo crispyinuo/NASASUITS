defb4e45191fc00c03a794ef3329fb7c8185fef2
Mcc <cucyzx@gmail.com>
2024-04-28 23:16:45 -0700

    NetworkManager update

--- Assets/Scripts/NetworkManager.cs
+++ Assets/Scripts/NetworkManager.cs
@@ -14,12 +14,15 @@ public class NetworkManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        webSocket = new WebSocket("ws://localhost:8080");
-        webSocket.Connect();
+        webSocket = new WebSocket("ws://f4e8-2600-387-f-6118-00-3.ngrok-free.app");
+        Debug.Log("Hey");
+        webSocket.OnOpen += (sender, e) => {Debug.Log("Opened connection!");};
+        webSocket.OnError += (sender, e) => {Debug.Log("Error: " + e.Message);};
         webSocket.OnMessage += (sender, e) => 
         {
             Debug.Log("Message Received from " + ((WebSocket)sender).Url + ", Data : " + e.Data);
         };
+        webSocket.Connect();
     }
 
     // Update is called once per frame
@@ -34,6 +37,7 @@ public class NetworkManager : MonoBehaviour
             UserCommand command = new UserCommand();
             command.user_input = "do task one a";
             webSocket.Send(JsonUtility.ToJson(command));
+            Debug.Log("Sent!");
         }
     }
 }
